c355d064a6816277a48961034df8337d7e2fe349
maksimvlasov <maks.mephi@gmail.com>
2022-04-29 22:34:50 +0300

    Add empty project

---
+++ src/main/java/com/example/responsebytes/ResponsebytesApplication.java
@@ -0,0 +1,13 @@
+package com.example.responsebytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponsebytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponsebytesApplication.class, args);
+	}
+
+}
